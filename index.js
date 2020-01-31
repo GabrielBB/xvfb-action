77b492818163490985f2ae1fb938a21868624d1c
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2020-01-31 14:46:24 -0400

    Removing @actions/github

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
     const command = core.getInput('run');
     console.log(`Command to execute: ${command}!`);
+
 } catch (error) {
     core.setFailed(error.message);
 }
