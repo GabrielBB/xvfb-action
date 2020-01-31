52245c34348ab378b2ab6ca027f366e1c6126d3f
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2020-01-31 14:57:10 -0400

    Running commands to install and execute xvfb

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 try {
     const command = core.getInput('run');
-    console.log(`Command to execute: ${command}!`);
-
+    
+    exec.exec("sudo apt-get install xvfb");
+    exec.exec("xvfb-run", ["--auto-servernum", command]);
 } catch (error) {
     core.setFailed(error.message);
 }
