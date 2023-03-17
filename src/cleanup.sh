6a74d4ec1f9e22ed6674d5c8feb855751a2697ec
Ian K Smith <smithki707@gmail.com>
2023-03-16 20:39:39 -0600

    replace build system

---
+++ src/cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#author Bret Comnes
+
+kill_xvfb () {
+    local xvfb_pids=`ps aux | grep tmp/xvfb-run | grep -v grep | awk '{print $2}'`
+    if [ "$xvfb_pids" != "" ]; then
+        echo "Killing the following xvfb processes: $xvfb_pids"
+        sudo kill $xvfb_pids
+    else
+        echo "No xvfb processes to kill"
+    fi
+}
+
+kill_xvfb
